cf0aaa422b329a4486ac578badd91da2b23206cb
frank <losoqq1@qq.com>
2013-09-05 00:13:57 +0800

    update edit page

--- public/javascripts/edit.js
+++ public/javascripts/edit.js
@@ -5,10 +5,13 @@
  * Time: 11:26 PM
  * To change this template use File | Settings | File Templates.
  */
+
 $(function($){
   $('#editFormBox02').toggle();
   $('.EditFormOption').click(function() {
     $(this).toggleClass('ButtonToggleClose ButtonToggleOpen');
     $('#editFormBox02').toggle('fast');
   });
-}($));
+  $('.WidgetInputBox').autosize({append: '\n'});
+//  $('.WidgetInputBox').expandingTextArea();
+});
