c5ecfe134f5e54298e8cdab93963b56e57a33f12
stefanzan <zantao007@gmail.com>
2013-09-27 22:06:50 +0900

    recover config.js

--- config.js
+++ config.js
@@ -5,7 +5,24 @@
  * Time: 1:23 AM
  * To change this template use File | Settings | File Templates.
  */
+
 module.exports = {
-  port: 3000,
-  db: 'mongodb://127.0.0.1/mtm'
+    debug: true,
+    name: "mtm",
+    host:"localhost.mtm.org",
+    port: 3000,
+    db: 'mongodb://127.0.0.1/mtm',
+
+    session_secret: 'mtm',
+
+    mail_opts : {
+        /* host: '' ,
+         port: 25, */
+        service: "Gmail",
+        auth: {
+            user: 'splatform2013@gmail.com',
+            pass: 'zongjie0924'
+        }
+
+    }
 }
