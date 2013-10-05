a23027fa0ea0e7b736f66ae7420c330de442fd64
stefanzan <zantao007@gmail.com>
2013-10-05 19:57:46 +0900

    add personal page select   button js support personal.js

--- public/javascripts/personalAccountManage/personal.js
+++ public/javascripts/personalAccountManage/personal.js
@@ -5,29 +5,25 @@
  * Time: 6:00 PM
  * To change this template use File | Settings | File Templates.
  */
-(function($) {
-  $( ".mdHeadUtil01Open" )
+
+$(function ($) {
+  $(".mdHeadUtil01Open")
     .button({
       text: false,
       icons: {
         primary: "ui-icon-triangle-1-s"
       }
     })
-    .click(function() {
-      console.log("on click");
-      var menu = $( this ).parent().next().show().position({
+    .click(function () {
+      $(this).next().toggle();
+      var menu = $(this).next().position({
         my: "left top",
         at: "left bottom",
         of: this
       });
-      $( document ).one( "click", function() {
+      $(document).one("click", function () {
         menu.hide();
       });
       return false;
     })
-    .parent()
-      .buttonset()
-      .next()
-        .hide()
-        .menu();
 });
