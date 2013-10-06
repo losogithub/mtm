5889c7e913922b5d27733a44c015bb1170c6178e
frank <losoqq1@qq.com>
2013-10-06 08:44:14 +0800

    fix:避免污染命名空间

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -5,7 +5,10 @@
  * Time: 12:00 AM
  * To change this template use File | Settings | File Templates.
  */
-var errorImage = function (img, name) {
+//使用全局变量应避免污染命名空间
+var mtm = window.mtm || {};
+
+mtm.errorImage = mtm.errorImage || function (img, name) {
   var url = '/images/no_img/' + name + '.png';
   if (url != $(img).attr('src')) {
     $(img).attr('src', url);
