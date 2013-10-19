56b8007dd8136c930fb3cb2e84e50bd212fe32ef
stefanzan <zantao007@gmail.com>
2013-10-19 15:48:39 +0900

    index  css ul.li problem

--- controllers/site.js
+++ controllers/site.js
@@ -20,14 +20,30 @@ var index = function (req, res, next) {
         des: topic.description
       });
     });
+    var DateObj = showDate();
     res.render('index', {
       title: 'mtm[我设计的信息。策展平台]',
       css: ['/stylesheets/index.css'],
       js: ['/javascripts/index.js'],
       pageType: 'INDEX',
+      dayInChn : DateObj.dayInChn,
+      today: DateObj.today,
+      today1: DateObj.today1,
       hot: topicsData
     });
   });
+}
+
+var showDate = function(){
+  var today = new Date();
+  console.log("------------today----------");
+  console.log(today);
+  var day = today.getDay();
+  var dayMap = {0 : "星期日", 1:"星期一", 2:"星期二", 3: "星期三", 4: "星期四", 5:"星期五", 6:"星期六"}
+  var dayInChn = dayMap[day];
+  var  showToday = today.getFullYear() + '.' + (today.getMonth() + 1) + "." + today.getDate();
+  var  showToday1 = showToday.replace('.', '-');
+  return {dayInChn: dayInChn, today: showToday, today1: showToday1};
 }
 
 exports.index = index;
