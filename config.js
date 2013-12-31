6c5b573354b746f16b2477863e5e9d0124b780b0
frank <losoqq1@qq.com>
2013-12-31 19:56:56 +0800

    fix:ie不能操作 embed的src问题

--- config.js
+++ config.js
@@ -18,15 +18,15 @@ module.exports = {
   port: '3000',
   db: 'mongodb://127.0.0.1:27017/mtm',
 
-    session_secret: 'shizier',
-    auth_cookie_name: 'shizierCookie',
-    mail_opts : {
-        host: 'shizier.com',
-        port: 25,
-        auth: {
-            user: 'notification@shizier.com',
-            pass: 'shizier0725'
-        }
+  session_secret: 'shizier',
+  auth_cookie_name: 'shizierCookie',
+  mail_opts: {
+    host: 'shizier.com',
+    port: 25,
+    auth: {
+      user: 'notification@shizier.com',
+      pass: 'shizier0725'
+    }
 
   }
 }
